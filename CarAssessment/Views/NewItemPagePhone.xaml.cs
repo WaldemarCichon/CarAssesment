9dbd991d33c0ecef7cb3670f0ebe69a5f9365df8
WaldemarCichon <waldemar@cisoft.de>
2021-12-08 01:34:34 +0100

    NewItemPagePhone introduced

---
+++ CarAssessment/Views/NewItemPagePhone.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CarAssessment.Views
+{
+    public partial class NewItemPagePhone : ContentPage
+    {
+        public NewItemPagePhone()
+        {
+            InitializeComponent();
+        }
+    }
+}
