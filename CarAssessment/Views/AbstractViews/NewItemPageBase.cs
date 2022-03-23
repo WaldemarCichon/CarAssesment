6726d562fe214f89e2a59b157cb28352612d8baa
waldi <waldemar.cichon.de@gmail.com>
2022-03-23 02:16:12 +0100

    added missing files

---
+++ CarAssessment/Views/AbstractViews/NewItemPageBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace CarAssessment.Views.AbstractViews {
+	public abstract class NewItemPageBase: ContentPage {
+		public NewItemPageBase() {
+		}
+
+		public abstract bool PrevArrowButtonVisiblity { get; set; }
+		public abstract bool NextArrowButtonVisiblity { get; set; }
+
+		internal abstract void HandleSpecialFields(int displayedGroup);
+
+		internal abstract void EnterDeclarationOfAssignment();
+
+
+		internal abstract void EnterAdvocateAssignment();
+	}
+}
